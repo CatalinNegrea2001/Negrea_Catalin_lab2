d4488a6a1b614c2ff564b7bf64a7b9d5aa9c6aec
Catalin Negrea <catalin.florin.negrea@stud.ubbcluj.ro>
2023-11-16 13:58:46 +0200

    Lab6

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,12 +7,17 @@ namespace Negrea_Catalin_lab2.Models
     public class Book
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength =3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
         public int? AuthorID { get; set; }
         public Authors? Author { get; set; }
 
         public string AuthorName => Author?.FirstName + " " + Author?.LastName;
+        [Column(TypeName = "decimal(6, 2")]
+        [Range(0.01, 500)]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
--- Program.cs
+++ Program.cs
@@ -4,9 +4,22 @@ using Negrea_Catalin_lab2.Data;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+    policy.RequireRole("Admin"));
+});
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Categories", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Publishers", "AdminPolicy");
+});
 builder.Services.AddDbContext<Negrea_Catalin_lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Negrea_Catalin_lab2Context") ?? throw new InvalidOperationException("Connection string 'Negrea_Catalin_lab2Context' not found.")));
 
@@ -16,6 +29,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("Negrea_Catalin_l
 ?? throw new InvalidOperationException("Connection string 'Negrea_Catalin_lab2Context' not found.")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
